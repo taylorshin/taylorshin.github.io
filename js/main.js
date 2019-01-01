33fc233e509bee780e06e2f78fb1ba8569d0a9d2
Taylor Shin <taylorshin@Taylors-MacBook-Pro.local>
2018-12-31 22:28:30 -0800

    Updates

--- js/main.js
+++ js/main.js
@@ -3,7 +3,16 @@ const aboutSection = document.getElementById('about');
 const portfolioSection = document.getElementById('portfolio');
 const contactSection = document.getElementById('contact');
 
-homeSection.addEventListener('click', homeSection.scrollIntoView({behavior: 'instant'}));
-aboutSection.addEventListener('click', aboutSection.scrollIntoView({behavior: 'instant'}));
-portfolioSection.addEventListener('click', portfolioSection.scrollIntoView({behavior: 'instant'}));
-contactSection.addEventListener('click', contactSection.scrollIntoView({behavior: 'instant'}));
+const homeLink = document.getElementById('home-link');
+const aboutLink = document.getElementById('about-link');
+const portfolioLink = document.getElementById('portfolio-link');
+const contactLink = document.getElementById('contact-link');
+
+// homeLink.addEventListener('click', scrollTo(homeSection));
+// aboutLink.addEventListener('click', scrollTo(aboutSection));
+// portfolioLink.addEventListener('click', scrollTo(portfolioSection));
+// contactLink.addEventListener('click', scrollTo(contactSection));
+
+function scrollTo(section) {
+    section.scrollIntoView({behavior: 'smooth', block: 'end', inline: 'nearest'});
+}
